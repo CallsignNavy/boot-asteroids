72251b4cd11c449ef93d73f17480956c4276ae4d
CallsignNavy <callsignnavy.business@gmail.com>
2025-07-28 17:17:03 -0400

    added groups into main and modified code

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from player import Player
 
 def main():
     pygame.init()
-
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -26,9 +26,10 @@ def main():
 
         screen.fill("black")
 
-        player.update(dt)
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
 
-        player.draw(screen)
         pygame.display.flip()
 
 
